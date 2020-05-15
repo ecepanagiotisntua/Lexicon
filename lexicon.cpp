bfa4f59eb0f78f8f916a4046d72590774c591508
ecepanagiotisntua <pkaoukis.2001@yahoo.gr>
2020-05-15 12:48:26 +0000

    a

--- lexicon.cpp
+++ lexicon.cpp
@@ -1,3 +1,40 @@
 #include <iostream>
 
 using namespace std;
+
+class lexicon {
+  struct node {
+      string word;
+      int times;
+      node *left, *right, *parent;
+      node(const string &s, node *l, node *r, node *p) 
+        : word(s), left(l), right(r), parent(p) {}
+    };
+
+    node *root;
+
+    static void purge(node *t) {
+      if(t != nullptr){
+        purge(t->left);
+        purge(t->right);
+        delete t;
+      }
+    }
+ public:
+ lexicon()
+ : root(nullptr){}
+ ~lexicon(){
+   purge(root);
+ }
+
+
+ void insert(const string &s);
+
+ int lookup(const string &s) const;
+
+ int depth(const string &s) const;
+ 
+ void replace(const string &s1, const string &s2);
+
+ friend ostream & operator << (ostream &out, const lexicon &l);
+};
