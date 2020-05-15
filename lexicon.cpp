db275bede097508ffdac236d2fcc13773d4b5108
ecepanagiotisntua <pkaoukis.2001@yahoo.gr>
2020-05-15 13:10:19 +0000

    a

--- lexicon.cpp
+++ lexicon.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class lexicon {
   struct node {
       string word;
-      int times;
+      int times = 1;
       node *left, *right, *parent;
       node(const string &s, node *l, node *r, node *p) 
         : word(s), left(l), right(r), parent(p) {}
@@ -20,6 +20,26 @@ class lexicon {
         delete t;
       }
     }
+
+    static bool insert(node *t, const T &x){
+      if (x < t->word){
+        if (t->left == nullptr){
+          t->left = new node(x, nullptr, nullptr, t);
+          return true;
+        } else
+          return insert(t->left, x);
+      } else if (x > t->word){
+        if (t->right == nullptr){
+          t->right = new node(x, nullptr, nullptr, t);
+          return true;
+        } else
+          return insert(t->right, x);
+
+      }
+     else if (x == t->word) t->times++;
+
+    }
+
  public:
  lexicon()
  : root(nullptr){}
@@ -28,7 +48,15 @@ class lexicon {
  }
 
 
- void insert(const string &s);
+ void insert(const string &s){
+   if (root == nullptr){
+     root = new node(s, nullptr, nullptr, nullptr);
+    }
+    else if(insert(root, s));
+
+ }
+
+ 
 
  int lookup(const string &s) const;
 
